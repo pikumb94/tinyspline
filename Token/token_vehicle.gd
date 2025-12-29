7868256a853443137dddd642457a925737a62f45
pikumb94 <pikumb94@gmail.com>
2025-12-29 17:07:51 +0100

    lots of assets

--- Token/token_vehicle.gd
+++ Token/token_vehicle.gd
@@ -3,10 +3,17 @@ extends Area3D
 @export var duration: float = 2.0
 @export var speed_multiplier: float = 1.0
 
+
+
 func _ready():
 	if speed_multiplier<0:
 		$CollisionShape3D.debug_color = Color.DARK_RED
 
+func _process(delta):
+	var cam := get_viewport().get_camera_3d()
+	if cam:
+		$Sprite3D.look_at(cam.global_position, Vector3.UP)
+		
 func _on_area_entered(area):
 	print(area)
 	pass # Replace with function body.
