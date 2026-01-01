244a875dfa3e226c19435bbd8f62f9cbecc0e995
pikumb94 <pikumb94@gmail.com>
2026-01-01 23:41:40 +0100

    done imporing tilest

--- Token/token_vehicle.gd
+++ Token/token_vehicle.gd
@@ -2,15 +2,16 @@ extends Area3D
 
 @export var duration: float = 2.0
 @export var speed_multiplier: float = 1.0
-
-
+@export var destroy_on_pickup: bool = true
 
 func _ready():
+	
 	if speed_multiplier<0:
 		$CollisionShape3D.debug_color = Color.DARK_RED
 
 func _process(delta):
 	var cam := get_viewport().get_camera_3d()
+	print(cam)
 	if cam:
 		$Sprite3D.look_at(cam.global_position, Vector3.UP)
 		
@@ -22,3 +23,5 @@ func _on_body_entered(body):
 	var vehicle = body.get_parent()
 	if vehicle is Vehicle:
 		vehicle.apply_speed_multiplier(duration, speed_multiplier)
+		if destroy_on_pickup:
+			queue_free()
